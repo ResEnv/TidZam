9f9e08c2d2ee984503fa9d523ecafa97ef8ba5a4
slash6475 <slash6475@duhart-clement.fr>
2016-04-05 12:12:07 +0200

    [FIX] Add Nothing classifier deployment during installation process

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@
 sudo apt-get install nodejs octave octave-audio   octave-control  octave-general  octave-signal sox vorbis-tools mplayer qarecord ffmpeg
 mkdir data tmp
 mkdir data/classifiers data/database data/dataset data/processed_records data/stream data/training
+cp Nothing/* data/classifiers
+cp Nothing/Nothing.dat data/dataset
 coffee --compile --output dist src
